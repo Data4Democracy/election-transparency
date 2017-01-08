351c3f4bfdfbda50f7545df0bea27e608ab60673
Scott Came <scottcame10@gmail.com>
2017-01-07 17:05:41 -0800

    Added FL

---
+++ r-packages/voterregus/R/fl.R
@@ -0,0 +1,28 @@
+#' @importFrom rvest html_nodes html_table
+#' @importFrom xml2 read_html
+#' @import dplyr
+#' @import tidyr
+#' @importFrom tibble as_tibble
+#' @export
+loadFlorida <- function() {
+
+  countyNameFIPSMapping <- getCountyNameFIPSMapping('12') %>%
+    mutate(CountyName=toupper(CountyName))
+
+  page <- read_html("http://dos.myflorida.com/elections/data-statistics/voter-registration-statistics/voter-registration-monthly-reports/voter-registration-current-by-county/")
+  tables <- page %>% html_nodes("table") %>% html_table(fill=TRUE)
+
+  df <- tables[[1]] %>%
+    rename(CountyName=COUNTY, R=REPUBLICAN, D=DEMOCRAT, O=MINOR, N=NONE) %>%
+    select(-TOTAL) %>%
+    mutate_each(funs(gsub(x=., pattern=",", replacement=""))) %>%
+    mutate(L=NA, G=NA) %>%
+    mutate_each("as.integer", -CountyName) %>%
+    as_tibble() %>%
+    filter(CountyName != 'TOTAL') %>%
+    left_join(countyNameFIPSMapping, by=c("CountyName"="CountyName")) %>%
+    select(-CountyName)
+
+  df
+
+}
--- r-packages/voterregus/data-raw/ExportData.R
+++ r-packages/voterregus/data-raw/ExportData.R
@@ -7,7 +7,8 @@ dfs <- list(
   voterregus::loadColorado(),
   voterregus::loadConnecticut(),
   voterregus::loadDelaware(),
-  voterregus::loadDC()
+  voterregus::loadDC(),
+  voterregus::loadFlorida()
 )
 
 PartyRegistration <- dplyr::select(dplyr::mutate(dplyr::bind_rows(dfs), State=substr(County, 1, 2)), State, County, D, G, L, N, O, R)
