635cc8c75186e6cd51f0ddd277e6ead7586bdd3f
Scott Came <scottcame10@gmail.com>
2017-01-08 08:34:25 -0800

    Optimize loading of county fips/name mapping

--- r-packages/voterregus/R/util.R
+++ r-packages/voterregus/R/util.R
@@ -1,6 +1,15 @@
 #' @importFrom rgdal readOGR
 #' @import dplyr
 getCountyNameFIPSMapping <- function(StateFIPSCode) {
-  county_shp <- readOGR("data-raw/tl_2014_us_county/", "tl_2014_us_county") %>% subset(.$STATEFP == StateFIPSCode)
-  county_shp@data %>% select(CountyName=NAME, County=GEOID) %>% mutate_each("as.character")
+
+  if (!exists("countyData", env=packageEnv)) {
+    assign("countyData", readOGR("data-raw/tl_2014_us_county/", "tl_2014_us_county")@data, env=packageEnv)
+  }
+
+  get("countyData", env=packageEnv) %>%
+    filter(STATEFP==StateFIPSCode) %>%
+    select(CountyName=NAME, County=GEOID) %>% mutate_each("as.character")
+
 }
+
+packageEnv <- new.env()
