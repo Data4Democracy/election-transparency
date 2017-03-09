ff0523515f14b3c4610d7b769e166c49836d9ed7
angelocioffi <angelo.cff@gmail.com>
2017-03-09 00:14:48 -0600

    Modified tx.R to collect 2014 registration data as well as 2016 data

--- r-packages/uselections/R/tx.R
+++ r-packages/uselections/R/tx.R
@@ -9,10 +9,10 @@ loadTexas <- function() {
   countyNameFIPSMapping <- getCountyNameFIPSMapping('48') %>%
     mutate(CountyName=toupper(CountyName))
 
-  page <- read_html("https://www.sos.state.tx.us/elections/historical/nov2016.shtml")
-  tables <- page %>% html_nodes("table") %>% html_table(fill=TRUE)
+  page.2016 <- read_html("https://www.sos.state.tx.us/elections/historical/nov2016.shtml")
+  tables.2016 <- page.2016 %>% html_nodes("table") %>% html_table(fill=TRUE)
 
-  df <- tables[[1]] %>%
+  df.2016 <- tables.2016[[1]] %>%
     filter(`County Name` != 'Statewide Total') %>%
     select(CountyName=`County Name`, N=`Voter Registration`) %>%
     mutate_each(funs(gsub(x=., pattern=",", replacement=""))) %>%
@@ -25,6 +25,25 @@ loadTexas <- function() {
     select(-CountyName) %>%
     as_tibble()
 
+  
+  page.2014 <- read_html("https://www.sos.state.tx.us/elections/historical/nov2014.shtml")
+  tables.2014 <- page.2014 %>% html_nodes("table") %>% html_table(fill=TRUE)
+  
+  df.2014 <- tables.2014[[1]] %>%
+    filter(`County Name` != 'Statewide Total') %>%
+    select(CountyName=`County Name`, N=`Voter Registration`) %>%
+    mutate_each(funs(gsub(x=., pattern=",", replacement=""))) %>%
+    mutate(D=NA, G=NA, L=NA, R=NA, O=NA) %>%
+    mutate(Year = 2014, Month = 11) %>% # Hardcode until we add historical data
+    select(CountyName, D, G, L, R, N, O, Year, Month) %>%
+    mutate_each("as.integer", -CountyName) %>%
+    mutate(CountyName=ifelse(CountyName=='LASALLE', 'LA SALLE', CountyName)) %>%
+    left_join(countyNameFIPSMapping, by=c("CountyName"="CountyName")) %>%
+    select(-CountyName) %>%
+    as_tibble()
+  
+  df <- rbind(df.2016, df.2014)
+  
   df
 
 }
